1355bf6d6c6881cc427857c568e47f0317f2e10f
marco raphael dantas campos <marco-raphael>
2022-05-10 17:25:33 -0300

    criacao do coordinator e routes

---
+++ futureEats/src/routes/Router.js
@@ -0,0 +1,40 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+
+                <Route>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router
